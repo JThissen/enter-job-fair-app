d4fd807df0d14d8ee0e9e35f0f2fd573c5a2dd41
Joost Thissen <joost.thissen@hotmail.com>
2020-05-13 18:14:02 +0200

    Removed unused CSS

--- enter-job-fair-form/src/app/app.module.ts
+++ enter-job-fair-form/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FormComponent } from './form/form.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
 import { FormConfirmComponent } from './form-confirm/form-confirm.component';
